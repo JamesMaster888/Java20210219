d79df15aa452d9196333dbf6f11c7486c3625d43
jamesliao <JamesCodingMaster@hotmail.com>
2021-02-21 15:21:52 +0800

    Java OCP

---
+++ src/main/java/com/mycompany/java20210219/day01/BMIDemo3.java
@@ -0,0 +1,8 @@
+package com.mycompany.java20210219.day01;
+public class BMIDemo3 {
+    public static void main(String[] args) {
+        BMIDemo2.printBMI("John", 170, 60);
+        BMIDemo2.printBMI("Mary", 160, 48);
+        BMIDemo2.printBMI("Helen", 166, 65);
+    }
+}
--- src/main/java/com/mycompany/java20210219/day01/EMIDemo.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.java20210219.day01;
-
-public class EMIDemo {
-    public static void main(String[] args)
-    {
-        System.out.println("hi");
-    }
-
-}
